e2f8b8d0e6abb1859d3d418bb5b83947612ce25a
DhivyaDharshiniT <dhivyadd2912@gmail.com>
2024-01-21 21:03:28 +0530

    javascript

---
+++ operators.js
@@ -0,0 +1,19 @@
+
+//Assignment operator
+var a=10
+console.log("Assignment operator otput: ")
+//Arithmetic operator
+var aA= 10
+var bA=0
+console.log("Arithmetic operator Addition operator ",aA+bA)
+//Logical operator --> &&,||,!
+console.log("Logical operator AND output",aA&&bA)
+console.log("Logical operator OR output",aA||bA)
+console.log("Logical operator ! output",aA&&bA)
+console.log("Logical operator  output",!aA)
+console.log(10&&20)
+console.log(10&&55)
+console.log(10||20)
+console.log(0||20)
+console.log(100||20&&0)
+console.log(100||20&&0)
